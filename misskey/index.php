196be5fbfad8753394f78a54ec1e3308c8334598
satopian <satopian@mia.eek.jp>
2025-01-15 00:25:52 +0900

    neo.js

--- misskey/index.php
+++ misskey/index.php
@@ -1,8 +1,8 @@
 <?php
-//Petit Note (c)さとぴあ @satopian 2021-2023
+//Petit Note (c)さとぴあ @satopian 2021-2025
 //1スレッド1ログファイル形式のスレッド式画像掲示板
 $petit_ver='for_misskey';
-$petit_lot='lot.20241225';
+$petit_lot='lot.20250114';
 $lang = ($http_langs = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '')
   ? explode( ',', $http_langs )[0] : '';
 $en= (stripos($lang,'ja')!==0);
